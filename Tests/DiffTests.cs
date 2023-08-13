a23aa4126e56357f7c4f72e7b490ac1990d19b34
Dmitriy Vornychev <dm.s.vornychev@outlook.com>
2023-08-14 00:31:39 +0200

    Additional diff test

--- Tests/DiffTests.cs
+++ Tests/DiffTests.cs
@@ -382,4 +382,36 @@ public class DiffTests
         Assert.That(diff.Blocks[5].OriginalLines, Is.EqualTo("FG".CharsToStringArray()));
         Assert.That(diff.Blocks[5].ModifiedLines, Is.EqualTo("34".CharsToStringArray()));
     }
+
+    [Test]
+    public void Test_17_MultipleMinorChangesWithTrailing()
+    {
+        var diff = Diff.Make(@base: "ABCDEFGH", modified: "A1C2E34H");
+
+        Assert.Multiple(
+            () =>
+        {
+            Assert.That(diff.Blocks, Has.Count.EqualTo(7));
+
+            Assert.That(diff.Blocks[0].Type, Is.EqualTo(BlockType.Unchanged));
+            Assert.That(diff.Blocks[1].Type, Is.EqualTo(BlockType.Changed));
+            Assert.That(diff.Blocks[2].Type, Is.EqualTo(BlockType.Unchanged));
+            Assert.That(diff.Blocks[3].Type, Is.EqualTo(BlockType.Changed));
+            Assert.That(diff.Blocks[4].Type, Is.EqualTo(BlockType.Unchanged));
+            Assert.That(diff.Blocks[5].Type, Is.EqualTo(BlockType.Changed));
+            Assert.That(diff.Blocks[6].Type, Is.EqualTo(BlockType.Unchanged));
+        });
+
+
+        Assert.That(diff.Blocks[0].OriginalLines, Is.EqualTo("A".CharsToStringArray()));
+        Assert.That(diff.Blocks[1].OriginalLines, Is.EqualTo("B".CharsToStringArray()));
+        Assert.That(diff.Blocks[1].ModifiedLines, Is.EqualTo("1".CharsToStringArray()));
+        Assert.That(diff.Blocks[2].OriginalLines, Is.EqualTo("C".CharsToStringArray()));
+        Assert.That(diff.Blocks[3].OriginalLines, Is.EqualTo("D".CharsToStringArray()));
+        Assert.That(diff.Blocks[3].ModifiedLines, Is.EqualTo("2".CharsToStringArray()));
+        Assert.That(diff.Blocks[4].OriginalLines, Is.EqualTo("E".CharsToStringArray()));
+        Assert.That(diff.Blocks[5].OriginalLines, Is.EqualTo("FG".CharsToStringArray()));
+        Assert.That(diff.Blocks[5].ModifiedLines, Is.EqualTo("34".CharsToStringArray()));
+        Assert.That(diff.Blocks[6].ModifiedLines, Is.EqualTo("H".CharsToStringArray()));
+    }
 }
