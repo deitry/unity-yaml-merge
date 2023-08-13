b34fde79b3e30aabce7f88e8079d960e4be01380
Dmitriy Vornychev <dm.s.vornychev@outlook.com>
2023-08-13 21:35:03 +0200

    WIP

--- Program.cs
+++ Program.cs
@@ -5,27 +5,70 @@ using YamlDotNet.Serialization.NamingConventions;
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-var deserializer = new DeserializerBuilder()
-    .WithTagMapping(new YamlDotNet.Core.TagName("tag:unity3d.com,2011:114"), typeof(LocalizationAsset))
-    .Build();
-
 var ours = args[0];
 var @base = args[1];
 var theirs = args[2];
+var outputPath = $"{ours}.converted";
 
-//yml contains a string containing your YAML
-var yml = File.ReadAllText(ours);
-var p = deserializer.Deserialize<LocalizationAsset>(yml);
+var oursYml = File.ReadAllLines(ours);
+var theirsYml = File.ReadAllLines(theirs);
 
-foreach (var item in p.MonoBehaviour.m_TableData)
+var oursI = 0;
+var theirsI = 0;
+
+using var output = File.CreateText(outputPath);
+var isNewEntry = (string line) => line.StartsWith("  - m_Id");
+
+// first is for shared asset with keys, second for localizations
+var isAssetEnd = (string line) => line == "  m_Metadata:" || line == "  references:";
+
+while (true)
 {
-    Console.WriteLine(item.m_Localized);
+    var oursLine = oursYml[oursI];
+    var theirsLine = theirsYml[theirsI];
+
+    if (oursLine == theirsLine)
+    {
+        oursI++;
+        theirsI++;
+
+        output.WriteLine(oursLine);
+        continue;
+    }
+
+    // if not equal
+
+    // write all new keys from ours file
+    if (isNewEntry(oursLine) && (isNewEntry(theirsLine) || isAssetEnd(theirsLine)))
+    {
+        while (isNewEntry(oursLine))
+        {
+            do
+            {
+                // write all lines until we find the next key
+                output.WriteLine(oursLine);
+                oursLine = oursYml[++oursI];
+            } while (!isNewEntry(oursLine) && !isAssetEnd(oursLine));
+        }
+
+        continue;
+    }
+
+    if (isNewEntry(theirsLine) && isAssetEnd(oursLine))
+    {
+        // write all new keys from theirs file
+        while (isNewEntry(theirsLine))
+        {
+            do
+            {
+                // write all lines until we find the next key
+                output.WriteLine(theirsLine);
+                theirsLine = theirsYml[++theirsI];
+            } while (!isNewEntry(theirsLine) && !isAssetEnd(theirsLine));
+        }
+
+        continue;
+    }
+
+    throw new Exception($"Lines are not equal:\n{oursLine}\n{theirsLine}");
 }
-
-// serialize back
-var serializer = new SerializerBuilder()
-    .WithTagMapping(new YamlDotNet.Core.TagName("tag:unity3d.com,2011:114"), typeof(LocalizationAsset))
-    .Build();
-
-var yaml = serializer.Serialize(p);
-File.WriteAllText($"{ours}.converted", yaml);
