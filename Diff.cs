d2c3fdd2818e7108c3280e8d1fd0c1f4fd39fffc
Dmitriy Vornychev <dm.s.vornychev@outlook.com>
2023-08-13 21:35:03 +0200

    WIP

---
+++ Diff.cs
@@ -0,0 +1,137 @@
+﻿namespace unity_yaml_merge;
+
+/// <summary>
+/// Difference between two files
+/// </summary>
+public class Diff
+{
+    public IReadOnlyList<Block> Blocks => _blocks;
+
+    private List<Block> _blocks = new ();
+
+    private BlockType? LastBlockType => _blocks.Any() ? _blocks.Last().Type : null;
+    private Diff()
+    {
+    }
+
+    /// <summary>
+    /// Create a diff between two files
+    /// </summary>
+    public static Diff Make(string[] @base, string[] modified)
+    {
+        var diff = new Diff();
+
+        if (!@base.Any() && !modified.Any())
+            return diff;
+
+        using var baseEnumerator = @base.GetTextEnumerator();
+        using var modifiedEnumerator = modified.GetTextEnumerator();
+
+        var baseEnded = false;
+        var modifiedEnded = false;
+
+        while (true)
+        {
+            if (!baseEnded)
+                baseEnded = !baseEnumerator.MoveNext();
+
+            if (!modifiedEnded)
+                modifiedEnded = !modifiedEnumerator.MoveNext();
+
+            if (baseEnded && modifiedEnded)
+                break;
+
+            BlockType? currentBlockType = null;
+            if (baseEnded && !modifiedEnded)
+                currentBlockType = BlockType.Added;
+            else if (baseEnumerator.Current == modifiedEnumerator.Current)
+                currentBlockType = BlockType.Unchanged;
+            else if (!baseEnded && modifiedEnded)
+                currentBlockType = BlockType.Removed;
+            // else
+            //     currentBlockType = BlockType.Changed;
+
+            if (currentBlockType.HasValue && (!diff._blocks.Any() || currentBlockType != diff.LastBlockType))
+            {
+                var newBlock = new Block(currentBlockType.Value, baseEnumerator.LineNumber, modifiedEnumerator.LineNumber);
+                diff._blocks.Add(newBlock);
+            }
+
+            if (!baseEnded && !modifiedEnded
+                && baseEnumerator.Current == modifiedEnumerator.Current)
+            {
+                diff._blocks.Last().OldValue.Add(baseEnumerator.Current);
+                diff._blocks.Last().NewValue.Add(modifiedEnumerator.Current);
+            }
+
+            if (baseEnded && !modifiedEnded)
+                diff._blocks.Last().NewValue.Add(modifiedEnumerator.Current);
+
+            if (!baseEnded && modifiedEnded)
+                diff._blocks.Last().OldValue.Add(baseEnumerator.Current);
+        }
+
+        return diff;
+    }
+}
+
+
+public enum BlockType
+{
+    Unchanged,
+    Changed,
+    Added,
+    Removed,
+}
+
+public class Block
+{
+    public BlockType Type { get; private set; }
+
+    public Block(BlockType currentBlockType, int startingOldLineNumber, int startingNewLineNumber)
+    {
+        Type = currentBlockType;
+        StartingOldLineNumber = startingOldLineNumber;
+        StartingNewLineNumber = startingNewLineNumber;
+    }
+
+    public List<string> OldValue { get; } = new ();
+    public List<string> NewValue { get; } = new ();
+
+    public int StartingOldLineNumber { get; private set; }
+    public int StartingNewLineNumber { get; private set; }
+}
+
+public static class EnumerableExtensions
+{
+    public static TextEnumerator GetTextEnumerator(this IEnumerable<string> enumerator)
+    {
+        return new TextEnumerator(enumerator);
+    }
+}
+
+public class TextEnumerator : IDisposable
+{
+    private readonly IEnumerator<string> _enumerator;
+    private int _lineNumber = 0;
+    private bool _hasNext = true;
+
+    public TextEnumerator(IEnumerable<string> enumerable)
+    {
+        _enumerator = enumerable.GetEnumerator();
+    }
+
+    public bool MoveNext()
+    {
+        _lineNumber++;
+        return _hasNext = _enumerator.MoveNext();
+    }
+
+    public string Current => _enumerator.Current;
+    public int LineNumber => _lineNumber;
+
+    public void Dispose()
+    {
+        _enumerator.Dispose();
+    }
+}
---
+++ Tests/DiffTests.cs
@@ -0,0 +1,102 @@
+﻿using unity_yaml_merge;
+
+namespace Tests;
+
+public class DiffTests
+{
+    [Test]
+    public void Test_01_Empty()
+    {
+        var diff = Diff.Make(new string []{}, new string []{});
+
+        Assert.That(diff.Blocks.Count, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void Test_02_EqualSingleLine()
+    {
+        var diff = Diff.Make(new []{ "A" }, new [] { "A" });
+
+        Assert.That(diff.Blocks.Count, Is.EqualTo(1));
+        Assert.That(diff.Blocks[0].Type, Is.EqualTo(BlockType.Unchanged));
+        Assert.That(diff.Blocks[0].OldValue, Is.EqualTo(new []{ "A" }));
+        Assert.That(diff.Blocks[0].NewValue, Is.EqualTo(new []{ "A" }));
+    }
+
+    [Test]
+    public void Test_02_AddedToEmpty()
+    {
+        var diff = Diff.Make(new string []{ }, new [] { "A" });
+
+        Assert.That(diff.Blocks.Count, Is.EqualTo(1));
+        Assert.That(diff.Blocks[0].Type, Is.EqualTo(BlockType.Added));
+        Assert.That(diff.Blocks[0].OldValue, Is.EqualTo(new string []{ }));
+        Assert.That(diff.Blocks[0].NewValue, Is.EqualTo(new []{ "A" }));
+    }
+
+    [Test]
+    public void Test_03_AddedLine()
+    {
+        var diff = Diff.Make(@base: new []{ "A" }, modified: new [] { "A", "B" });
+
+        Assert.That(diff.Blocks.Count, Is.EqualTo(2));
+
+        Assert.That(diff.Blocks[0].Type, Is.EqualTo(BlockType.Unchanged));
+        Assert.That(diff.Blocks[1].Type, Is.EqualTo(BlockType.Added));
+
+        Assert.That(diff.Blocks[0].NewValue, Is.EqualTo(new []{ "A" }));
+        Assert.That(diff.Blocks[1].OldValue, Is.EqualTo(new string []{ }));
+        Assert.That(diff.Blocks[1].NewValue, Is.EqualTo(new []{ "B" }));
+    }
+
+    [Test]
+    public void Test_04_AddedLineInTheMiddle()
+    {
+        var diff = Diff.Make(@base: new []{ "A", "C" }, modified: new [] { "A", "B", "C" });
+
+        Assert.That(diff.Blocks.Count, Is.EqualTo(3));
+
+        Assert.That(diff.Blocks[0].Type, Is.EqualTo(BlockType.Unchanged));
+        Assert.That(diff.Blocks[1].Type, Is.EqualTo(BlockType.Added));
+        Assert.That(diff.Blocks[2].Type, Is.EqualTo(BlockType.Unchanged));
+
+        Assert.That(diff.Blocks[0].NewValue, Is.EqualTo(new []{ "A" }));
+        Assert.That(diff.Blocks[1].OldValue, Is.EqualTo(new string []{ }));
+        Assert.That(diff.Blocks[1].NewValue, Is.EqualTo(new []{ "B" }));
+        Assert.That(diff.Blocks[2].NewValue, Is.EqualTo(new []{ "C" }));
+    }
+
+    [Test]
+    public void Test_05_RemovedLineInTheMiddle()
+    {
+        var diff = Diff.Make(@base: new []{ "A", "B", "C" }, modified: new [] { "A", "C" });
+
+        Assert.That(diff.Blocks.Count, Is.EqualTo(3));
+
+        Assert.That(diff.Blocks[0].Type, Is.EqualTo(BlockType.Unchanged));
+        Assert.That(diff.Blocks[1].Type, Is.EqualTo(BlockType.Removed));
+        Assert.That(diff.Blocks[2].Type, Is.EqualTo(BlockType.Unchanged));
+
+        Assert.That(diff.Blocks[0].NewValue, Is.EqualTo(new []{ "A" }));
+        Assert.That(diff.Blocks[1].OldValue, Is.EqualTo(new []{ "B" }));
+        Assert.That(diff.Blocks[1].NewValue, Is.EqualTo(new string []{ }));
+        Assert.That(diff.Blocks[2].NewValue, Is.EqualTo(new []{ "C" }));
+    }
+
+    [Test]
+    public void Test_06_ChangedLineInTheMiddle()
+    {
+        var diff = Diff.Make(@base: new []{ "A", "B", "C" }, modified: new [] { "A", "D", "C" });
+
+        Assert.That(diff.Blocks.Count, Is.EqualTo(3));
+
+        Assert.That(diff.Blocks[0].Type, Is.EqualTo(BlockType.Unchanged));
+        Assert.That(diff.Blocks[1].Type, Is.EqualTo(BlockType.Changed));
+        Assert.That(diff.Blocks[2].Type, Is.EqualTo(BlockType.Unchanged));
+
+        Assert.That(diff.Blocks[0].NewValue, Is.EqualTo(new []{ "A" }));
+        Assert.That(diff.Blocks[1].OldValue, Is.EqualTo(new []{ "B" }));
+        Assert.That(diff.Blocks[1].NewValue, Is.EqualTo(new []{ "D" }));
+        Assert.That(diff.Blocks[2].NewValue, Is.EqualTo(new []{ "C" }));
+    }
+}
