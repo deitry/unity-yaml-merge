c63def99eb40dbbb357633ec59023f676a2a4399
Dmitriy Vornychev <dm.s.vornychev@outlook.com>
2023-08-14 13:44:03 +0200

    Fix Main

--- Program.cs
+++ Program.cs
@@ -4,15 +4,15 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-
         var ours = args[0];
         var @base = args[1];
         var theirs = args[2];
 
         var merged = Merger.MergeYamls(ours, @base, theirs);
-
-        using var output = File.CreateText($"{ours}.converted");
+        
+        // we should overwrite ours file
+        // https://git-scm.com/docs/gitattributes#_defining_a_custom_merge_driver
+        using var output = File.CreateText(ours);
         foreach (var line in merged)
         {
             output.WriteLine(line);
